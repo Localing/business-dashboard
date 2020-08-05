b5b2db164cec62465c4cfba7d639c575ebc3eb13
Vishnu Ravi <vishnu.ravi@gmail.com>
2020-08-05 11:17:08 -0400

    Connect auth to AWS amplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { UserProvider } from './UserContext';
+
+// Pages
 import Home from './components/Home';
+
+// Initialize AWS Amplify
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
 function App() {
   return (
--- src/UserContext.tsx
+++ src/UserContext.tsx
@@ -1,34 +1,106 @@
 import React, { useState, useEffect } from 'react';
-import { Auth } from 'aws-amplify';
+import { Auth, Hub } from 'aws-amplify';
 
-const UserContext = React.createContext({
-    loading: true,
-    authenticated: false,
-    user: null
-});
+type UserContextType = {
+    loading: boolean,
+    authenticated: boolean,
+    user: object | null,
+    login: (user: string, password: string) => void,
+    logout: () => void,
+    loginError: object | null
+}
+
+const UserContext = React.createContext<UserContextType | undefined>(undefined);
 
 export const UserConsumer = UserContext.Consumer;
 
 export const UserProvider: React.FC = ({ children }) => {
 
-    const [loading, setLoading] = useState(true);
-    const [authenticated, setAuthenticated] = useState(false);
-    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [authenticated, setAuthenticated] = useState<boolean>(false);
+    const [user, setUser] = useState<object | null>(null);
+    const [loginError, setLoginError] = useState<object | null>(null);
+
+    const login = async (email: string, password: string) => {
+        requestLogin();
+        try {
+            const user = await Auth.signIn(email, password);
+            receiveLogin(user);
+        } catch (err) {
+            receiveLoginError(err);
+        }
+    }
+
+    const logout = async () => {
+        try {
+            await Auth.signOut();
+            receiveLogout();
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    const requestLogin = () => {
+        setUser(null);
+        setAuthenticated(false);
+        setLoading(true);
+        setLoginError(null);
+    }
+
+    const receiveLogin = (user: object) => {
+        setUser(user);
+        setAuthenticated(true);
+        setLoading(false);
+        setLoginError(null);
+    }
+
+    const receiveLoginError = (error: object) => {
+        setLoginError(error);
+        setUser(null);
+        setAuthenticated(false);
+        setLoading(false);
+    }
+
+    const receiveLogout = () => {
+        setUser(null);
+        setAuthenticated(false);
+        setLoading(false);
+    }
+
+    const getCurrentUser = async () => {
+        requestLogin();
+        try {
+            const user = await Auth.currentAuthenticatedUser();
+            receiveLogin(user);
+        } catch (error) {
+            receiveLogout();
+        }
+    }
 
     useEffect(() => {
-        const getCurrentUser = async () => {
-            try {
-                const user = await Auth.currentAuthenticatedUser();
-                setUser(user);
-                setAuthenticated(true);
-                setLoading(false);
-            } catch (error) {
-                // no user is logged in
-                setAuthenticated(false);
-                setLoading(false);
-            }
-        }
+        // check for a logged in user
         getCurrentUser();
+
+        // listen for amplify auth events
+        Hub.listen('auth', async (data) => {
+            switch (data.payload.event) {
+                case 'signIn':
+                    Auth.currentAuthenticatedUser({
+                        bypassCache: true
+                    }).then(user => receiveLogin(user))
+                        .catch(err => console.log(err));
+                    break;
+                case 'signIn_failure':
+                    receiveLoginError(data.payload.data);
+                    break;
+                case 'signOut':
+                    receiveLogout();
+                    break;
+                default:
+                    break;
+            }
+        })
+
     }, []);
 
     return (
@@ -36,7 +108,10 @@ export const UserProvider: React.FC = ({ children }) => {
             value={{
                 loading,
                 authenticated,
-                user
+                user,
+                loginError,
+                login,
+                logout
             }}>
             {children}
         </UserContext.Provider>
