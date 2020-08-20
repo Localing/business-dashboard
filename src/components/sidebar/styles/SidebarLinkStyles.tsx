98aca0540ca3b47382ad43425a85507798e13e78
Cameron Robey <cameron@cameronrobey.co.uk>
2020-08-20 23:45:08 +0100

    New sidebar look styles

--- src/components/sidebar/styles/SidebarLinkStyles.tsx
+++ src/components/sidebar/styles/SidebarLinkStyles.tsx
@@ -8,17 +8,49 @@ interface NavigationLinkProps {
 }
 
 // Component styles
-export const NavigationLink = styled(({linkActive, ...rest}) => <Link {...rest} />)<NavigationLinkProps>`
-  color: ${ ({linkActive}) => (linkActive ? "#466ed4" : "#666666")};
-`;
-
 export const NavigationIcon = styled(FontAwesomeIcon)`
   display: inline-block;
   margin-right: 10px;
+  transition: color 0.5s ease;
+`;
+
+export const NavigationLink = styled(({linkActive, ...rest}) => <Link {...rest} />)<NavigationLinkProps>`
+  color: #fff;
+  background-image: linear-gradient(to left, #007bff 50%, rgba(0,0,0,0) 50%);
+  background-size: 200% 100%;
+
+  ${ ({linkActive}) => (linkActive ? `background-position: -100% 0%;` : ``)}
+
+  border-radius: 3px;
+  padding: 10px 20px;
+  margin: 2px 0;
+  transition: background-position 0.5s ease;
+
+  & > ${NavigationIcon} {
+    color: ${ ({linkActive}) => (linkActive ? "#fff" : "#48576c")}; 
+  }
+  
+  &:hover {
+    ${ ({linkActive}) => (linkActive ? `
+    ` : `
+    background-position: -100% 0%;
+    `)}
+
+    & > ${NavigationIcon} {
+      ${ ({linkActive}) => (linkActive ? `
+      ` : `
+      color: #fff !important;
+      `)}
+    }
+  }
 `;
 
 export const NavigationText = styled.p`
   display: inline-block;
+  margin: 0;
+  font-weight: 600;
+  font-size: 1rem;
+  color: #fff;
 
   @media (max-width: 720px) {
     font-size: 1.2rem;
--- src/components/sidebar/styles/SidebarStyles.tsx
+++ src/components/sidebar/styles/SidebarStyles.tsx
@@ -9,9 +9,8 @@ interface SidebarProps {
 
 // Component styles
 export const Sidebar = styled(({sidebarShow, ...rest}) => <Col {...rest}/>)`
-  background-color: #f8f9fa;
+  background-color: #283142;
   height: 100vh;
-  border-right: 2px #dddddd solid;
   position: fixed;
 
   width: calc( 100vw * (1/6));
@@ -44,6 +43,7 @@ export const BusinessName = styled.p`
   font-size: 1.4rem;
   margin: 0;
   padding: 20px 20px;
+  color: #fff;
 
   @media (max-width: 720px) {
     padding-top: 40px;
@@ -53,7 +53,8 @@ export const BusinessName = styled.p`
 export const NavigationSection = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 10px 20px;
+  padding: 10px 0;
+  width: 100%;
 `;
 
 export const SidebarScroll = styled.div`
@@ -64,11 +65,33 @@ export const SidebarScroll = styled.div`
 `;
 
 export const LogoutIcon = styled(FontAwesomeIcon)`
+  transition: color 0.5s ease;
 `;
 
 export const LogoutText = styled.p`
+  color: #fff;
+  background-image: linear-gradient(to left, #007bff 50%, rgba(0,0,0,0) 50%);
+  background-size: 200% 100%;
+
+  border-radius: 3px;
+  padding: 10px 20px;
+  margin: 2px 0;
+  transition: background-position 0.5s ease;
+
+  & > ${LogoutIcon} {
+    color: #48576c; 
+  }
+  
+  &:hover {
+    background-position: -100% 0%;
+
+    & > ${LogoutIcon} {
+      color: #fff !important;
+    }
+  }
+
   cursor: pointer;
-  color: #666666;
+  color: #fff;
   margin: 0;
   @media (max-width: 720px) {
     font-size: 1.2rem;
@@ -79,5 +102,6 @@ export const MobileLogout = styled.div`
   display: flex;
   align-items: center;
   height: 10vh;
+  width: 100%;
 `;
 
