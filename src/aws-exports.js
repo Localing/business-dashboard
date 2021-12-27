510ffd46a880514650c2332553b0743b99ddb440
Vishnu Ravi <vishnu.ravi@gmail.com>
2021-12-27 10:51:00 -0500

    Update config

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,8 +1,8 @@
 const awsmobile = {
     "aws_project_region": "eu-west-2",
     "aws_cognito_region": "eu-west-2",
-    "aws_user_pools_id": "eu-west-2_NzD6u4BOr",
-    "aws_user_pools_web_client_id": "u4gqkt9qlkah8k9bifans9u6k",
+    "aws_user_pools_id": "",
+    "aws_user_pools_web_client_id": "",
     "oauth": {
         "domain": "auth.localing.co.uk",
         "scope": [
